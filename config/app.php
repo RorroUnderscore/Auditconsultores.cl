8f61d2d300550f0452648812e281b90931ce9bf9
RorroUnderscore <143210581+RorroUnderscore@users.noreply.github.com>
2026-05-07 19:38:16 -0400

    Add use-template and scratch questionnaire creation flows

---
+++ config/app.php
@@ -0,0 +1,15 @@
+<?php
+return [
+  'db' => [
+    'connection' => 'mysql', // mysql | sqlite
+    'host' => 'localhost',
+    'port' => '3306',
+    'database' => 'tupresen_audit360',
+    'username' => 'tupresen_audituser',
+    'password' => 'CAMBIAR_PASSWORD',
+  ],
+  'admin_seed' => [
+    'email' => 'admin@auditconsultores.cl',
+    'password' => 'CAMBIAR_PASSWORD_ADMIN',
+  ],
+];
